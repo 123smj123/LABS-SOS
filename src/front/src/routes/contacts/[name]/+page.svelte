4e431646cca2f12cded90534becd35bbcec7956c
Santiago <123smj123@gmail.com>
2026-03-27 10:58:08 +0100

    Final commit

--- src/front/src/routes/contacts/[name]/+page.svelte
+++ src/front/src/routes/contacts/[name]/+page.svelte
@@ -1,6 +1,6 @@
 <script>
     import { page } from '$app/state';
-    	import { dev } from '$app/environment';
+    import { dev } from '$app/environment';
 	import { onMount } from 'svelte';
 
 	let API = '/api/v1/contacts';
